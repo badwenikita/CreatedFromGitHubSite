1491a7fafbae2d90166f3c811e22656e25930bc3
badwenikita <badwe.nikita@gmail.com>
2020-03-09 17:24:32 +0530

    Commit is used to add changes to the local repository

---
+++ src/test/java/CreatedFromGitHubSite/CFGS/TestCase1.java
@@ -0,0 +1,13 @@
+package CreatedFromGitHubSite.CFGS;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 
+{
+	@Test
+	public void method1()
+	{
+		System.out.println("method1 added from Eclipse");
+	}
+
+}
